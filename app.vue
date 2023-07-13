2ca49b94fa7f85569b48f59b3c0a5b1610ecd122
yuKalasnikova2 <pmalian@list.ru>
2023-07-13 13:10:03 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+
+ <template>
+  <div class="flex">
+    <Header />
+    <div class="content-wrapper">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.content-wrapper {
+  margin: 0 auto;
+  background: pink;
+}
+</style>
