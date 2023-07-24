850aa00cd6cc36f314d2a3c492fe70fbfbbb469d
yuKalasnikova2 <pmalian@list.ru>
2023-07-24 14:44:08 +0700

    feat(component): HeroInformation

--- components/HeroBlock.vue
+++ components/HeroBlock.vue
@@ -2,19 +2,11 @@
 
 <template>
   <div class="hero">
-    <div class="hero-info">
-      <div class="hero-info__text">
-        <h2 class="hero-info__title">
-          Luxury homeware for people who love timeless design quality
-        </h2>
-        <span class="hero-info__description"
-          >Shop the new Spring 2022 collection today</span
-        >
-      </div>
-      <div class="hero-info__link">
-        <NuxtLink to="#" class="hero-info__link-btn">View collection</NuxtLink>
-      </div>
-    </div>
+    <HeroInformation
+      title="Luxury homeware for people who love timeless design quality"
+      description="Shop the new Spring 2022 collection today"
+      link="#"
+    />
   </div>
 </template>
 
@@ -27,30 +19,5 @@
   justify-content: flex-end;
   align-items: center;
   padding-right: 80px;
-  &-info {
-    background-color: #fff;
-    max-width: 630px;
-    padding: 48px 55px;
-    height: 444px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    &__title {
-      line-height: 1.5;
-      font-weight: 400;
-      margin: 0 0 20px 0;
-    }
-    &__link {
-      background: #f9f9f9;
-      width: 170px;
-      display: flex;
-      justify-content: center;
-      &-btn {
-        color: #2a254b;
-        padding: 16px 32px;
-      }
-    }
-  }
 }
 </style>
---
+++ components/HeroInformation.vue
@@ -0,0 +1,77 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  link: {
+    type: String,
+    required: false,
+  },
+  colorTitle: {
+    type: String,
+    default: '',
+  },
+  colorDescription: {
+    type: String,
+    default: '#5B5676',
+  },
+  background: {
+    type: String,
+    default: '#fff',
+  },
+})
+</script>
+<template>
+  <div class="hero-info" :style="{ backgroundColor: background }">
+    <div class="hero-info__text">
+      <h2 class="hero-info__title" :style="{ color: colorTitle }">
+        {{ title }}
+      </h2>
+      <span
+        class="hero-info__description"
+        :style="{ color: colorDescription }"
+        >{{ description }}</span
+      >
+    </div>
+    <div class="hero-info__link">
+      <NuxtLink :to="link" class="hero-info__link-btn"
+        >View collection</NuxtLink
+      >
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.hero {
+  &-info {
+    background-color: #fff;
+    max-width: 630px;
+    padding: 48px 55px;
+    height: 444px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    &__title {
+      line-height: 1.5;
+      font-weight: 400;
+      margin: 0 0 20px 0;
+    }
+    &__link {
+      background: #f9f9f9;
+      width: 170px;
+      display: flex;
+      justify-content: center;
+      &-btn {
+        color: #2a254b;
+        padding: 16px 32px;
+      }
+    }
+  }
+}
+</style>
