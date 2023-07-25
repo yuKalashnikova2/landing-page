d8ecaeee68a417b3feb96458e896ed438328ee84
yuKalasnikova2 <pmalian@list.ru>
2023-07-25 09:08:21 +0700

    feat(component): Subscribe

---
+++ components/Subscribe.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="subscribe">
+    <div class="subscribe-content">
+      <h3 class="subscribe-title">Join the club and get the benefits</h3>
+      <span class="subscribe-description"
+        >Sign up for our newsletter and receive exclusive offers on new ranges,
+        sales, pop up stores and more</span
+      >
+
+      <div class="subscribe-benefits">
+        <div class="subscribe-benefits__item">
+          <img src="/svg/subscribe-check.svg" />
+          <span>Exclusive offers</span>
+        </div>
+        <div class="subscribe-benefits__item">
+          <img src="/svg/subscribe-check.svg" />
+          <span>Large discounts</span>
+        </div>
+        <div class="subscribe-benefits__item">
+          <img src="/svg/subscribe-check.svg" />
+          <span>Free events</span>
+        </div>
+      </div>
+
+      <form class="subscribe-form">
+        <input
+          type="text"
+          placeholder="your@email.com"
+          class="input-text subscribe-form__input"
+        />
+        <ButtonUI color-btn="primary">Sign up</ButtonUI>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.subscribe {
+  background: url('/img/subscribe.jpg');
+  padding: 97px 0 86px 0;
+  background-size: cover;
+  color: #fff;
+  &-content {
+    max-width: 500px;
+    margin: auto;
+    text-align: center;
+  }
+
+  &-title {
+    display: block;
+    font-size: 32px;
+    font-weight: 400;
+    margin: 0 0 12px 0;
+  }
+  &-benefits {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+    & > *:not(:last-child) {
+      margin-right: 36px;
+    }
+    &__item {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+  &-form {
+    display: flex;
+    margin-top: 40px;
+    width: 100%;
+    &__input {
+      flex-grow: 1;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,5 @@
     <AboutBrand />
     <Products />
     <Features />
+    <Subscribe />
 </template>
